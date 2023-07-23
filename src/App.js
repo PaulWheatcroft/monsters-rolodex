133fa9398f07a5fa0790c14d603c8568f0053d8b
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-07-23 18:58:07 +0100

    Lesson 50

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox
+          className={'monsters-search-box'}
           onChangeHandler={onSearchChange}
           placeholder={'Search monster'}
         />
